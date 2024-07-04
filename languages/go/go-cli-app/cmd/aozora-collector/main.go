871836ed4e47f5a10b20b53ccecc0b13822ae57f
yuichi04 <crgone.910428.en@gmail.com>
2024-07-04 09:43:51 +0900

    ZIPファイルの展開プログラムを作成

--- languages/go/go-cli-app/cmd/aozora-collector/main.go
+++ languages/go/go-cli-app/cmd/aozora-collector/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
@@ -10,6 +14,7 @@ import (
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/text/encoding/japanese"
 )
 
 type Entry struct {
@@ -110,15 +115,63 @@ func findAuthorAndZIP(siteURL string) (string, string) {
 	return author, u.String()
 }
 
+func extractText(zipURL string) (string, error) {
+	resp, err := http.Get(zipURL)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	r, err := zip.NewReader(bytes.NewReader(b), int64(len(b)))
+	for _, file := range r.File {
+		if path.Ext(file.Name) == ".txt" {
+			f, err := file.Open()
+
+			if err != nil {
+				return "", err
+			}
+
+			b, err := io.ReadAll(f)
+			f.Close()
+
+			if err != nil {
+				return "", err
+			}
+
+			b, err = japanese.ShiftJIS.NewDecoder().Bytes(b)
+
+			if err != nil {
+				return "", err
+			}
+
+			return string(b), nil
+		}
+	}
+	return "", errors.New("contents not found")
+}
+
 func main() {
 	listURL := "https://www.aozora.gr.jp/index_pages/person879.html"
 
 	entries, err := findEntries(listURL)
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	for _, entry := range entries {
-		fmt.Println(entry.Title, entry.ZipURL)
+		content, err := extractText(entry.ZipURL)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		fmt.Println(entry.SiteURL)
+		fmt.Println(content)
 	}
 }
 
--- languages/go/go-cli-app/go.mod
+++ languages/go/go-cli-app/go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/PuerkitoBio/goquery v1.9.2 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
 	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
 )
