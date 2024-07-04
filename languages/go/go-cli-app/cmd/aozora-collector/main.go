2fa31093d2f84bd83db81ac25d7cd1c373be1ac6
yuichi04 <crgone.910428.en@gmail.com>
2024-07-04 09:13:41 +0900

    ZIPファイルのURL一覧を取得するプログラムを作成

--- languages/go/go-cli-app/cmd/aozora-collector/main.go
+++ languages/go/go-cli-app/cmd/aozora-collector/main.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"path"
 	"regexp"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -14,13 +17,13 @@ type Entry struct {
 	Author   string
 	TitleID  string
 	Title    string
-	InfoURL  string
+	SiteURL  string
 	ZipURL   string
 }
 
-func findEntries(siteURL string) ([]Entry, error) {
+func getResopnseBody(url string) (*goquery.Document, error) {
 	// URL から HTTP GET リクエストを実行
-	resp, err := http.Get(siteURL)
+	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -33,6 +36,12 @@ func findEntries(siteURL string) ([]Entry, error) {
 
 	// レスポンスボディを goquery で解析
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
+
+	return doc, err
+}
+
+func findEntries(siteURL string) ([]Entry, error) {
+	doc, err := getResopnseBody(siteURL)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,19 +49,65 @@ func findEntries(siteURL string) ([]Entry, error) {
 	// URL パターンをコンパイル
 	pat := regexp.MustCompile(`.*/cards/([0-9]+)/card([0-9]+).html$`)
 
+	entries := []Entry{}
+
 	// 解析したドキュメントを使用して、必要なデータを抽出
 	doc.Find("ol li a").Each(func(n int, elem *goquery.Selection) {
 		token := pat.FindStringSubmatch(elem.AttrOr("href", ""))
 		if len(token) != 3 {
 			return
 		}
+		title := elem.Text()
 		pageURL := fmt.Sprintf("https://www.aozora.gr.jp/cards/%s/card%s.html",
 			token[1], token[2])
-		println(pageURL)
+		author, zipURL := findAuthorAndZIP(pageURL) // 作者とZIPファイルのURLを取得
+
+		if zipURL != "" {
+			entries = append(entries, Entry{
+				AuthorID: token[1],
+				Author:   author,
+				TitleID:  token[2],
+				Title:    title,
+				SiteURL:  siteURL,
+				ZipURL:   zipURL,
+			})
+		}
 	})
 
-	// 処理
-	return nil, nil // ひとまず両方 nil を返しておく
+	return entries, nil
+}
+
+func findAuthorAndZIP(siteURL string) (string, string) {
+	doc, err := getResopnseBody(siteURL)
+	if err != nil {
+		return "", ""
+	}
+
+	author := doc.Find("table[summary=作家データ] tr:nth-child(1) td:nth-child(2)").Text()
+
+	zipURL := ""
+	doc.Find("table.download a").Each(func(n int, elem *goquery.Selection) {
+		href := elem.AttrOr("href", "")
+		if strings.HasSuffix(href, "zip") {
+			zipURL = href
+		}
+	})
+
+	if zipURL == "" {
+		return author, ""
+	}
+
+	if strings.HasPrefix(zipURL, "http://") || strings.HasPrefix(zipURL, "https://") {
+		return author, zipURL
+	}
+
+	u, err := url.Parse(siteURL)
+	if err != nil {
+		return author, ""
+	}
+
+	u.Path = path.Join(path.Dir(u.Path), zipURL)
+	return author, u.String()
 }
 
 func main() {
@@ -66,3 +121,9 @@ func main() {
 		fmt.Println(entry.Title, entry.ZipURL)
 	}
 }
+
+/*
+Go でうまくプログラムを作る上で重要なポイント
+- 最終的な処理は深く呼び出した関数の中では行わない
+- エラーが起きた場合はその場で強制終了せず main に戻す
+*/
