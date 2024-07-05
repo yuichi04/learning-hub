4e17e7a924c27a86bedc2fdc649d0a9ee7a5453c
yuichi04 <crgone.910428.en@gmail.com>
2024-07-06 07:12:04 +0900

    CLIアプリ作成

--- languages/go/go-cli-app/cmd/aozora-collector/main.go
+++ languages/go/go-cli-app/cmd/aozora-collector/main.go
@@ -115,6 +115,8 @@ func getResopnseBody(url string) (*goquery.Document, error) {
 	return doc, err
 }
 
+var pageURLFormat = "https://www.aozora.gr.jp/cards/%s/card%s.html"
+
 func findEntries(siteURL string) ([]Entry, error) {
 	doc, err := getResopnseBody(siteURL)
 	if err != nil {
@@ -133,8 +135,7 @@ func findEntries(siteURL string) ([]Entry, error) {
 			return
 		}
 		title := elem.Text()
-		pageURL := fmt.Sprintf("https://www.aozora.gr.jp/cards/%s/card%s.html",
-			token[1], token[2])
+		pageURL := fmt.Sprintf(pageURLFormat, token[1], token[2])
 		author, zipURL := findAuthorAndZIP(pageURL) // 作者とZIPファイルのURLを取得
 
 		if zipURL != "" {
